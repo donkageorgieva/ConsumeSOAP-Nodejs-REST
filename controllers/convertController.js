b633f311b7c2665af6637e07edf952ec3150ebf1
donkageorgieva <DoniGeorgieva16@gmail.com>
2022-05-18 17:02:40 +0300

    Provide Rest Services

--- controllers/convertController.js
+++ controllers/convertController.js
@@ -1,44 +1,21 @@
-import soap from "soap";
-import express from "express";
-const wsdlUrl = "http://localhost:53738/Convert.asmx?WSDL";
-
 export const gramToOunce = (req, res) => {
-  soap.createClient(wsdlUrl, function (err, client) {
-    client.GramToOunce({ "gram": req.params["grams"] }, function (err, result) {
-      res.send({
-        ounces: result.GramToOunceResult,
-      });
-    });
+  res.send({
+    ounces: req.params["grams"] / 28.34952,
   });
 };
 export const ounceToGram = (req, res) => {
-  soap.createClient(wsdlUrl, function (err, client) {
-    client.OunceToGram(
-      { "ounce": req.params["ounces"] },
-      function (err, result) {
-        res.send({
-          grams: result.OunceToGramResult,
-        });
-      }
-    );
+  res.send({
+    grams: req.params["ounces"] * 28.34952,
   });
 };
 export const kgToLb = (req, res) => {
-  soap.createClient(wsdlUrl, function (err, client) {
-    client.KgToLb({ "kg": req.params["kgs"] }, function (err, result) {
-      res.send({
-        lbs: result.KgToLbResult,
-      });
-    });
+  res.send({
+    lbs: req.params["kgs"] * 2.2,
   });
 };
 
 export const lbToKg = (req, res) => {
-  soap.createClient(wsdlUrl, function (err, client) {
-    client.LbToKg({ "lb": req.params["lbs"] }, function (err, result) {
-      res.send({
-        lbs: result.LbToKgResult,
-      });
-    });
+  res.send({
+    kgs: req.params["lbs"] / 2.2,
   });
 };
