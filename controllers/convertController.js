673dea1efe31fd54b30dff6c0e81b7b6569469b1
donkageorgieva <DoniGeorgieva16@gmail.com>
2022-05-19 11:35:54 +0300

    Cleanup

--- controllers/convertController.js
+++ controllers/convertController.js
@@ -1,5 +1,4 @@
 import soap from "soap";
-import express from "express";
 const wsdlUrl = "http://convert-service.somee.com/Convert.asmx?WSDL";
 
 export const gramToOunce = (req, res) => {
@@ -43,25 +42,3 @@ export const lbToKg = (req, res) => {
     });
   });
 };
-
-// export const gramToOunce = (req, res) => {
-//   res.send({
-//     ounces: req.params["grams"] / 28.34952,
-//   });
-// };
-// export const ounceToGram = (req, res) => {
-//   res.send({
-//     grams: req.params["ounces"] * 28.34952,
-//   });
-// };
-// export const kgToLb = (req, res) => {
-//   res.send({
-//     lbs: req.params["kgs"] * 2.2,
-//   });
-// };
-
-// export const lbToKg = (req, res) => {
-//   res.send({
-//     kgs: req.params["lbs"] / 2.2,
-//   });
-// };
